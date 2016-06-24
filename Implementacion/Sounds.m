bb0e881addc4bf86bbdc5fa067367ce2b9c27275
Santiago <santi_0926@hotmail.com>
2016-06-24 05:32:50 -0300

    Version final del proyecto

--- Implementacion/Sounds.m
+++ Implementacion/Sounds.m
@@ -1,24 +1,24 @@
 clear;
 %Prueba a 1 metro
 
-filename1 = '/media/santiago/Stuff/Carrera/5 Año/9° Cuatrimestre/PDS_Recursado/Proyecto/Sonidos/Prueba_1m.wav';
+filename1 = '/media/santiago/Stuff/Carrera/5 Año/9° Cuatrimestre/PDS_Recursado/Proyecto/Sonidos/Temperatura/20°/Prueba_1m_20grados3.wav';
 Calcule_Distance(filename1,1);
 
 %Pruebas a 2 metros
 
-filename2 = '/media/santiago/Stuff/Carrera/5 Año/9° Cuatrimestre/PDS_Recursado/Proyecto/Sonidos/Prueba_2m1.wav';
-Calcule_Distance(filename2,1);
+filename2 = '/media/santiago/Stuff/Carrera/5 Año/9° Cuatrimestre/PDS_Recursado/Proyecto/Sonidos/Temperatura/20°/Prueba_2m_20grados3.wav';
+%Calcule_Distance(filename2,1);
 
-filename3 = '/media/santiago/Stuff/Carrera/5 Año/9° Cuatrimestre/PDS_Recursado/Proyecto/Sonidos/Prueba_2m2.wav';
-Calcule_Distance(filename3,1);
+filename3 = '/media/santiago/Stuff/Carrera/5 Año/9° Cuatrimestre/PDS_Recursado/Proyecto/Sonidos/Temperatura/20°/Prueba_2m_20grados4.wav';
+%Calcule_Distance(filename3,1);
 
 %Pruebas a 1.5 m
 
-filename4 = '/media/santiago/Stuff/Carrera/5 Año/9° Cuatrimestre/PDS_Recursado/Proyecto/Sonidos/Prueba_15m.wav';
-Calcule_Distance(filename4,1);
+filename4 = '/media/santiago/Stuff/Carrera/5 Año/9° Cuatrimestre/PDS_Recursado/Proyecto/Sonidos/Temperatura/20°/Prueba_15m_20grados2.wav';
+%Calcule_Distance(filename4,1);
 
 %Prueba artificial
 
-x = Chirp_Generator(0);
-pure_sample = Chirp_Sample(x,2,0.5,0,0);
-Calcule_Distance(pure_sample,0);
+%x = Chirp_Generator(0);
+%pure_sample = Chirp_Sample(x,2,0.5,0,0);
+%Calcule_Distance(pure_sample,0);
